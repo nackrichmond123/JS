05d6274eedc80ad3963761c19e5c6ebdad40dc53
Nack Richmond <nackrichmond123@gmail.com>
2026-04-21 04:08:29 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// let num = "";
+// for(let i  = 0; i <= 10; i++){
+//     num += i;
+
+// }
+
+// console.log(num);
+
+// let element = document.createElement("h1");
+// element.innerHTML = "qwerty";
+// element.style.textAlign = "center";
+
+// document.body.appendChild(element);
+
+// const User1 = {
+//     username: "Khalifa"
+// }
+
+
+// let Check = () =>{
+//     let name = document.getElementById("name").value;
+
+//     if (name == User1.username ) {
+//         document.getElementById("show").innerHTML = name;
+//     }else{
+//         document.getElementById("show").innerHTML = "Name mismatch";
+//     }
+
+// }
+
+console.log(43+43);
